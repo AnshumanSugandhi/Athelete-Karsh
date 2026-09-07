daaf433e2bd0af3dcfd800b1ae6f07225873b313
Anshuman <anshuman.sugandhi7816@gmail.com>
2026-09-07 18:40:22 +0530

    chore: Prepare backend for Render deployment

--- athlete_platform/core/settings.py
+++ athlete_platform/core/settings.py
@@ -106,12 +106,13 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 # 2. Tell allauth which site ID to use
 SITE_ID = 1
